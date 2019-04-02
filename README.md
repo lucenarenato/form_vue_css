71910eed07c017da5ca318e9557df84f09ce566f
Renato de Oliveira Lucena <cpdrenato@gmail.com>
2019-04-02 01:08:20 -0300

    mudançcas

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 
 # vue-form
 
+<p align="center"><img src="Captura de tela_2019-03-29_15-14-35.png"></p>
+
+Basico html e css
+
+<br>
+
+<p align="center"><img src="Captura de tela de 2019-04-02 01-02-29.png"></p>
+
+- http://renatolucena.net
 
 - Renato Lucena 2019
 
--- js/index.js
+++ js/index.js
@@ -7,6 +7,11 @@ new Vue({
       email: "",
       log: ""
   },
+  computed: {
+          isValid: function () {
+            return this.name != '' && this.email != ''
+          }
+        },
   
   methods: {
       
@@ -17,9 +22,17 @@ new Vue({
             this.log = "Preencha o campo para nome.";
             event.preventDefault();
           }else{
+
             this.log = "Gravado";
-          }   
-      }
+            window.location.href = 'ok.html';
+
+            
+        }   
+      },
+      handleSubmit() {
+      // Send data to the server or update your stores and such.
+    }
+     
   }
 
   
@@ -28,13 +41,20 @@ new Vue({
 // grab the things we need
 var signupForm = document.getElementById('signup-form');
 var nameInput = signupForm.querySelector('input[name=name]');
+var telefoneInput = signupForm.querySelector('input[name=telefone]');
 var emailInput = signupForm.querySelector('input[name=email]');
+
 // listen for the submit event
 signupForm.addEventListener('submit', processSignupForm);
+
 function processSignupForm(e) {
     e.preventDefault();
     var name = nameInput.value;
+    var telefone = telefoneInput.value;
     var email = emailInput.value;
-    console.log({ name: name, email: email });
+    console.log({ name: name, telefone: telefone, email: email });  
+                        
     alert('Processing!');
+    return;
+    
 }
